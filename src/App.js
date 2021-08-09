5c9193d818698c8521cd9edc5ef3f01301988c43
HdzMarco1 <eh76396@gmail.com>
2021-08-09 14:36:53 -0600

    conversor de dividad

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import './App.css';
 
 
 function App() {
-  const [unidad, setUnidad] = useState(0);
-  const [decena, setDecena] = useState(0);
-  const [centena, setCentena] = useState(0);
-  const [umil, setUmil] = useState(0);
-  const [dmil, setDmil] = useState(0);
-  const [cmil, setCmil] = useState(0);
+  const [Dolar, setUnidad] = useState(0);
+  const [Euro, setDecena] = useState(0);
+  const [Bitcoin, setCentena] = useState(0);
 
   const [tipo, setTipo] = useState(1);
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
     setUnidad((total * tipo) / 1);
-    setDecena((total * tipo) / 10);
-    setCentena((total * tipo) / 100);
-    setUmil((total * tipo) / 1000);
-    setDmil((total * tipo) / 10000);
-    setCmil((total * tipo) / 100000);
+    setDecena((total * tipo) / 1.17);
+    setCentena((total * tipo) / 46400.41);
   }, [total, tipo]);
 
 
@@ -34,24 +28,18 @@ function App() {
     <div className="App">
       <h1>Convertidor de unidades</h1>
 
-      <p>Unidad: {unidad}</p>
-      <p>Decena: {decena}</p>
-      <p>Centena: {centena}</p>
-      <p>Unidad Mil: {umil}</p>
-      <p>Decena Mil: {dmil}</p>
-      <p>Centena Mil: {cmil}</p>
+      <p>Dolares: {Dolar}</p>
+      <p>Euros: {Euro}</p>
+      <p>Bitcoin: {Bitcoin}</p>
 
       <hr />
 
       <h3>{total}</h3>
 
       <select onChange={event => setTipo(event.target.value)} value={tipo}>
-        <option value={1}>Unidad</option>
-        <option value={10}>Decena</option>
-        <option value={100}>Centena</option>
-        <option value={1000}>Unidad Mil</option>
-        <option value={10000}>Decena Mil</option>
-        <option value={100000}>Centena Mil</option>
+        <option value={1}>Dolares</option>
+        <option value={1.17}>Euros</option>
+        <option value={46400.41}>Bitcoin</option>
       </select>
       <input onChange={handleTotalChange} value={total} />
     </div>
